fd7f4fd1f73af450e52850e3b2cb66bbb4a9fc8e
Maxime Wacker <maxime.wacker@free.fr>
2024-08-07 09:20:47 +0200

    Orientation change Animated Sharing same videoplyer to keep reading progress

--- VideoFullScreenOnLandscapePOC/ContentView.swift
+++ VideoFullScreenOnLandscapePOC/ContentView.swift
@@ -5,20 +5,55 @@
 //  Created by maxime wacker on 07/08/2024.
 //
 
-import SwiftUI
+// From: https://swiftuirecipes.com/blog/swiftui-device-orientation-change-observer
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+
+import SwiftUI
+import AVKit
+
+struct OrientationDetector: ViewModifier {
+  @Binding var orientation: UIDeviceOrientation
+    
+  func body(content: Content) -> some View {
+    content
+      .onReceive(NotificationCenter.default.publisher(for: UIDevice.orientationDidChangeNotification)) { _ in
+        orientation = UIDevice.current.orientation
+      }
+  }
+}
+
+extension View {
+  func detectOrientation(_ binding: Binding<UIDeviceOrientation>) -> some View {
+    self.modifier(OrientationDetector(orientation: binding))
+  }
+}
+
+struct OrientationTest: View {
+  @State private var orientation = UIDevice.current.orientation
+    let url = URL(
+        string: "https://storage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+    )!
+
+    let videoPlayer = VideoPlayer(player: AVPlayer(url: URL(
+        string: "https://storage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+    )!))
+  var body: some View {
+    Group {
+        if orientation.isLandscape {
+            videoPlayer
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .padding()
+                .background(.green)
+                .edgesIgnoringSafeArea(.all)
+        } else {
+            videoPlayer
+      }
     }
+    .detectOrientation($orientation)
+  }
 }
 
 #Preview {
-    ContentView()
+    OrientationTest()
+        .previewInterfaceOrientation(.landscapeLeft)
 }
--- VideoFullScreenOnLandscapePOC/VideoFullScreenOnLandscapePOCApp.swift
+++ VideoFullScreenOnLandscapePOC/VideoFullScreenOnLandscapePOCApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct VideoFullScreenOnLandscapePOCApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            OrientationTest()
         }
     }
 }
