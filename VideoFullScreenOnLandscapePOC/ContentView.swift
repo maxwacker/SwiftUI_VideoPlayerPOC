0b19404d1c76375ee28440293a9c50523092ba3a
Maxime Wacker <maxime.wacker@free.fr>
2024-08-07 08:15:50 +0200

    Initial Commit

---
+++ VideoFullScreenOnLandscapePOC/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VideoFullScreenOnLandscapePOC
+//
+//  Created by maxime wacker on 07/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VideoFullScreenOnLandscapePOC/VideoFullScreenOnLandscapePOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoFullScreenOnLandscapePOCApp.swift
+//  VideoFullScreenOnLandscapePOC
+//
+//  Created by maxime wacker on 07/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct VideoFullScreenOnLandscapePOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
